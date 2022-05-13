e04c66e4f4830bcc019990f8825558fc9aa9c261
daniel_hurtado <a01707774@itesm.mx>
2022-05-13 03:26:29 -0500

    Validate if box is occupied and determinate a winner

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -21,13 +21,24 @@ from turtle import pencolor
 from turtle import pensize
 from turtle import seth
 from turtle import fd
+from turtle import Screen
+from turtle import mainloop
 
-from freegames import line
+
+screen = Screen()
+screen.setup(800, 800)
+screen.title("Tic Tac Toe - PythonTurtle.Academy")
+screen.setworldcoordinates(-5, -5, 5, 5)
+screen.bgcolor('light gray')
+screen.tracer(0, 0)
+hideturtle()
 
 
 def grid():
-    """Draw tic-tac-toe grid."""
-    
+
+    # Draw tic-tac-toe grid.
+
+    # Set line color to green
     pencolor('green')
     pensize(10)
     up()
@@ -53,46 +64,93 @@ def grid():
 
 
 def drawx(x, y):
-    """Draw X player"""
-   
-    color('red')
-   
-    line(x, y, x + 13.3, y + 13.3)
-    line(x, y + 13.3, x + 13.3, y)
+
+    # Draw X player
+
+    # Change color to blue
+    color('blue')
+    up()
+    goto(x-0.5,y-0.5)
+    down()
+    goto(x+0.5,y+0.5)
+    up()
+    goto(x-0.5,y+0.5)
+    down()
+    goto(x+0.5,y-0.5)
 
 
 def drawo(x, y):
-    """Draw O player."""
-    
-    # Change color to blue
-    color('blue')
+   
+    # Draw O player.
     
     up()
-    goto(x + 6.7, y + 0.5)
+    goto(x,y-0.5)
+    seth(0)
+
+    # Change color to red
+    color('red')
     down()
-    circle(6.2)
+    circle(0.5, steps=100)
 
 
-def floor(value):
-    """Round value down to grid with square size 133."""
-    return ((value + 13.3) // 133) * 133 - 13.3
+def draw_piece(i, j, p):
+    if p == 0: return
+    x, y = 2*(j-1), -2*(i-1)
+    if p == 1:
+        drawx(x, y)
+    else:
+        drawo(x, y)
+    
+
+def draw(b):
+    grid()
+    for i in range(3):
+        for j in range(3):
+            draw_piece(i,j,b[i][j])
+    screen.update()
 
 
-state = {'player': 0}
-players = [drawx, drawo]
+# return 1 if player 1 wins, 2 if player 2 wins, 3 if tie, 0 if game is'nt over
+def gameover(b):
+    if b[0][0] > 0 and b[0][0] == b[0][1] and b[0][1] == b[0][2]: return b[0][0]
+    if b[1][0] > 0 and b[1][0] == b[1][1] and b[1][1] == b[1][2]: return b[1][0]
+    if b[2][0] > 0 and b[2][0] == b[2][1] and b[2][1] == b[2][2]: return b[2][0]
+    if b[0][0] > 0 and b[0][0] == b[1][0] and b[1][0] == b[2][0]: return b[0][0]
+    if b[0][1] > 0 and b[0][1] == b[1][1] and b[1][1] == b[2][1]: return b[0][1]
+    if b[0][2] > 0 and b[0][2] == b[1][2] and b[1][2] == b[2][2]: return b[0][2]
+    if b[0][0] > 0 and b[0][0] == b[1][1] and b[1][1] == b[2][2]: return b[0][0]
+    if b[2][0] > 0 and b[2][0] == b[1][1] and b[1][1] == b[0][2]: return b[2][0]
+    p = 0
+    for i in range(3):
+        for j in range(3):
+            p += (1 if b[i][j] > 0 else 0)
+    if p == 9: return 3
+    else: return 0
+    
 
-
-def tap(x, y):
-    """Draw X or O in tapped square."""
-    x = floor(x)
-    y = floor(y)
-
-    player = state['player']
-    draw = players[player]
-    draw(x, y)
-    update()
-    state['player'] = not player
-
+def play(x, y):
+    global turn
+    i = 3-int(y+5)//2
+    j = int(x+5)//2 - 1
+    if i>2 or j>2 or i<0 or j<0 or b[i][j]!=0: return
+    if turn == 'x': b[i][j], turn = 1, 'o'
+    else: b[i][j], turn = 2, 'x'
+    draw(b)
+    r = gameover(b)
+    if r == 1:
+        screen.textinput("Game over!","X won!")
+    elif r == 2:
+        screen.textinput("Game over!","O won!")
+    elif r == 3:
+        screen.textinput("Game over!", "Tie!")
+    
+# Initialize value of matrix
+    
+b = [ [ 0,0,0 ], [ 0,0,0 ], [ 0,0,0 ] ]    
+draw(b)
+turn = 'x'
+screen.onclick(play)
+mainloop()
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
